0af35e7ea549dd28603d957e85a790f74e664df5
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-05-10 22:32:27 +0300

    refresh width and height logo img

--- src/components/app-header/Header.jsx
+++ src/components/app-header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const clazz = 'mr-2 sm:mr-4'
   return (
     <header className="flex items-center justify-between w-full px-2 lg:px-0 min-h-[50px]">
-      <img className="w-18 h-7" src={logo} alt="logo" />
+      <img className="w-[100px] h-[30px]" src={logo} alt="logo" />
       <div className="text-slate-600">
         <Button clazz={clazz}>Sign In</Button>
         <Button>Sign Up</Button>
